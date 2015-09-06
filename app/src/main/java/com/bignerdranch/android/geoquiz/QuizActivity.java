e42b5dc83e8cb7f48d04ca8e7d9ec12bfd76d61b
noahpatterson <n@noahpatterson.me>
2015-09-06 17:28:37 -0400

    link up next button and questionTextView

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -7,12 +7,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private static final String LOG_TAG = "main activity";
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,10 @@ public class QuizActivity extends AppCompatActivity {
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +51,18 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getBaseContext(), R.string.toast_incorrect, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex != mQuestionBank.length - 1) {
+                    mCurrentIndex += 1;
+                    mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+                } else {
+                    Toast.makeText(getBaseContext(), R.string.end_of_quiz, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
